583fbf0e08f3d17f5badda0991c78c2c7db5a091
Tomiwa <akinyede8080@gmiail.com>
2024-08-25 23:13:21 +0100

    i added content to the RenderBlocks created a server for the cover block so it can display on the frontend

---
+++ src/blocks/cover/Server.tsx
@@ -0,0 +1,12 @@
+//this is the react component shown for our cover block
+
+import React from 'react'
+
+export default function CoverBlockServer({ title, subtitle }) {
+  return (
+    <div className="max-w-5xl py-20 text-center">
+      <h1 className="text-3xl font-bold">{title}</h1>
+      <h2 className="text-xl">{subtitle}</h2>
+    </div>
+  )
+}
---
+++ src/utils/RenderBlocks.tsx
@@ -0,0 +1,43 @@
+import CoverBlockServer from '@/blocks/cover/Server'
+import { Page } from '@/payload-types'
+import React, { Fragment } from 'react'
+//: { [key: string]: React.ComponentType<any> }
+//this is where we map slugs to the react component
+const blockComponents = {
+  cover: CoverBlockServer,
+}
+
+export const RenderBlocks: React.FC<{
+  blocks: Page['layout'][0][]
+}> = (props) => {
+  const { blocks } = props
+
+  const hasBlocks = blocks && Array.isArray(blocks) && blocks.length > 0
+
+  if (hasBlocks) {
+    return (
+      <Fragment>
+        {blocks.map((block, index) => {
+          const { blockName, blockType } = block
+
+          if (blockType && blockType in blockComponents) {
+            const Block = blockComponents[blockType]
+
+            if (Block) {
+              return (
+                <div className="my-16" key={index}>
+                  <Block id={blockName} {...block} />
+                </div>
+              )
+            }
+          }
+          return null
+        })}
+      </Fragment>
+    )
+  }
+
+  return null
+}
+//NB: it goes through the page layout for every block in the page layout if the block is in the block component it then assigns the block to the block component
+// our cover block will receive the heading prop, title block, subtitle props
